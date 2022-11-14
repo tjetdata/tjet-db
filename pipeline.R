3afe648d0693622abb373ebde9c80c8e6dd40554
Timo Thoms <timo@geeky.net>
2022-11-14 07:38:39 -0500

    Airtable download

--- pipeline.R
+++ pipeline.R
@@ -1,10 +1,11 @@
 # remotes::install_github('matthewjrogers/rairtable', ref = 'dev')
+### development versions handles multi-select fields without error
 library(tidyverse)
 library(rairtable)
 library(RSQLite)
 
 ### the api key needs to be set only once, and is saved to the local R environment
-### if it needs to be set again, replace string below accordingly
+### if it needs to be set again, replace string below accordingly and uncomment
 # set_airtable_api_key("ACTUAL_API_KEY_GOES_HERE", install = TRUE)
 # readRenviron("~/.Renviron")
 # Sys.getenv("AIRTABLE_API_KEY")
@@ -13,18 +14,20 @@ library(RSQLite)
 base_id <- "appHsoHAemKITZgMF"
 to_download <- c("Amnesties" = "amnesties_table_full", 
   "Trials" = "trials_table_full", 
-  # "Accused" = "accusd_table_full", 
+  "Accused" = "accused_table_full",
   "Truth Commissions" = "tcs_table_full", 
   "Reparations" = "reparations_table_full", 
   "Countries" = "countries_table",
   "Transitions" = "cy_table",
   "metadata" = "metadata_table")
-tjet <- lapply(names(to_download)[8], function(table) {
-  airtable(table, 'appHsoHAemKITZgMF', view = to_download[table]) %>%
+tjet <- lapply(names(to_download), function(table) {
+  airtable(table, base_id, view = to_download[table]) %>%
     read_airtable(id_to_col = TRUE)
 })
+save(tjet, file = "tjet.RData")
+# load("tjet.RData")
 
-# amnesty <- read_airtable(amnesty, id_to_col = TRUE)
-# sort(names(amnesty))
-# class(amnesty$whoWasAmnestied[1:10])
-# class(amnesty[1:10, c("whoWasAmnestied", "whatCrimes")])
+## have to deal with 
+## - multi-select fields: either turn into binary in Airtable or transform in R
+## - linked record fields: make sure the needed data is included, as linked record fields downlaod as the record identifiers 
+
