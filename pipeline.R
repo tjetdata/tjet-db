fe015090b17f82021024521ee1687a7ee1dfa4a3
Timo Thoms <timo@geeky.net>
2022-11-15 08:59:51 -0500

    added RSQLite sample code

--- pipeline.R
+++ pipeline.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(rairtable)
 library(RSQLite)
+library(parallel)
 
 ### the api key needs to be set only once, is saved to the local R environment
 ### if it needs to be set again, replace string and un-comment below accordingly
@@ -13,18 +14,24 @@ library(RSQLite)
 ### download data from Airtable 
 base_id <- "appHsoHAemKITZgMF"
 to_download <- c(
-  "Amnesties" = "export", 
-  "Trials" = "export", 
+  "Amnesties" = "export",
+  "Trials" = "export",
   "Accused" = "export",
-  "Truth Commissions" = "export", 
-  "Reparations" = "export", 
-  "Country-Years" = "cy_table", 
-  "Countries" = "countries_table", 
-  "metadata" = "metadata_table") 
+  "Truth Commissions" = "export",
+  "Reparations" = "export",
+  "Country-Years" = "cy_table",
+  "Countries" = "countries_table",
+  "metadata" = "metadata_table")
 tjet <- lapply(names(to_download), function(table) {
   airtable(table, base_id, view = to_download[table]) %>%
     read_airtable(id_to_col = TRUE)
 })
+names(tjet) <- str_replace(
+  str_replace(
+    str_to_lower(
+      names(to_download)
+    ), fixed("-"), "")
+  , fixed(" "), "")
 save(tjet, file = "tjet.RData")
 # load("tjet.RData")
 
@@ -34,8 +41,24 @@ save(tjet, file = "tjet.RData")
 ## - multi-select fields: either turn into binary in Airtable or transform in R
 ## - linked record fields: make sure the needed data is included, 
 ##   as linked record fields download as the record identifiers 
+## - variable transformations 
+##   - binary variables from checkbox fields
+## - clean up dataframes so that they can be added to the sqlite db 
+## - then complete below
 
-## variable transformations 
-## - binary variables from checkbox fields
+## create SQLite DB
+# file.remove("tjet.db")
+conn <- dbConnect(RSQLite::SQLite(), "tjet.db")
+lapply(names(tjet)[7:8], function(table_name) {
+  print(table_name)
+  dbWriteTable(conn, table_name, tjet[[table_name]])
+})
+dbListTables(conn) 
+dbListFields(conn, "metadata")
+dbReadTable(conn, "metadata")
+dim(dbGetQuery(conn, "SELECT * FROM metadata"))
 
-## create and save SQLite DB
+result <- dbSendQuery(conn, "SELECT * FROM metadata")
+dbFetch(result)
+dbClearResult(result)
+dbDisconnect(conn)
