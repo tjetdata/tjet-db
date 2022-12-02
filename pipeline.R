99c7f2277034750c66dced8c91fd52d9355f9272
Timo Thoms <timo@geeky.net>
2022-12-01 20:31:30 -0500

    transformations (wip)

--- pipeline.R
+++ pipeline.R
@@ -1,7 +1,5 @@
 # devtools::install_github("bergant/airtabler")
 # library(airtabler)
-# library(dplyr)
-
 # remotes::install_github('matthewjrogers/rairtable', ref = 'dev')
 ### development version handles multi-select fields without error
 library(tidyverse)
@@ -17,78 +15,86 @@ library(parallel)
 
 ### download data from Airtable 
 base_id <- "appHsoHAemKITZgMF"
-to_download <- c(
-  "Amnesties" = "export",
-  "Trials" = "export",
-  "Accused" = "export",
-  "Truth Commissions" = "export",
-  "Reparations" = "export",
-  "Country-Years" = "cy_table",
-  "Countries" = "countries_table",
-  "metadata" = "metadata_table")
-tjet <- lapply(names(to_download), function(table) {
-  airtable(table, base_id, view = to_download[table]) %>%
+names(to_download) <- to_download <- c(
+  "Amnesties",
+  "Trials",
+  "Accused",
+  "TruthCommissions",
+  "Reparations",
+  "Vettings",
+  "CountryYears",
+  "Countries",
+  "Transitions",
+  "Conflicts",
+  "Dyads",
+  "select_options",
+  "metadata")
+tjet[13] <- lapply(to_download[13], function(table) {
+  cat(table, "\n") 
+  airtable(table, base_id) %>%
     read_airtable(id_to_col = TRUE)
 })
-rename_labels <- function(names_to_change) {
-  str_replace(
-    str_replace(
-      str_to_lower(
-        names_to_change
-      ), fixed("-"), "")
-    , fixed(" "), ""
-  )
-}
-names(tjet) <- rename_labels(names(to_download))
+# rename_labels <- function(names_to_change) {
+#   str_replace(
+#     str_replace(
+#       str_to_lower(
+#         names_to_change
+#       ), fixed("-"), "")
+#     , fixed(" "), ""
+#   )
+# }
+# names(tjet) <- rename_labels(names(to_download))
 save(tjet, file = "tjet.RData")
 # load("tjet.RData")
 
-
 ### subsetting tables
-db <- lapply(names(tjet)[-8], function(tab_name) {
+db <- map(names(tjet)[-length(tjet)], function(tab_name) {
     # cat(tab_name, "\n", sep = "")
     select_vars <- tjet$metadata %>%
-      mutate(table_name = rename_labels(table_name)) %>%
-      filter(incl_prod == 1 & incl_data != "decide" & 
-             table_name == tab_name) %>%
+      filter(incl_prod == 1 & table_name == tab_name) %>%
       select(field_name) %>%
       unlist(use.names = FALSE)
-    # print(select_vars[!(select_vars %in% names(tjet[[tab_name]]))])
     missing_cols <- select_vars[!(select_vars %in% names(tjet[[tab_name]]))]
-    tjet[[tab_name]] %>% 
+    tjet[[tab_name]] %>%
       ### adding empty fields as NA columns for now
       mutate(!!!setNames(rep(NA, length(missing_cols)), missing_cols)) %>%
-      select(all_of(select_vars)) %>% 
+      select(all_of(select_vars)) %>%
       tibble()
   })
-names(db) <- names(tjet)[-8]
+names(db) <- names(tjet)[-length(tjet)]
+# object.size(db)
 
-### variable transformations of checkboxes and multiselects
+### checkboxes to binary
 checkbox_to_binary <- function(col) {
   ifelse(is.na(col), 0, 1)
 }
-make_named_list <- function(lst) {
-  if(!is.null(lst)) 
-    names(lst) <- lst %>% 
-      str_replace_all(fixed(" "), "_")
-  return(lst)
-}
+names(tables) <- tables <- names(db)[-length(db)]
+db[tables] <- map(tables, function(tab) {
+  fields <- tjet$metadata %>%
+    filter(incl_prod == 1 & 
+             incl_data == "checkmark to binary" & 
+             table_name == tab) %>%
+    select(field_name) %>% 
+    unlist(use.names = FALSE)
+  db[[tab]][, fields] <- map(db[[tab]][, fields], checkbox_to_binary)
+  return(db[[tab]])
+})
+
+### multiselects
+# make_named_list <- function(lst) {
+#   if(!is.null(lst)) 
+#     names(lst) <- lst %>% 
+#       str_replace_all(fixed(" "), "_")
+#   return(lst)
+# }
 
 ### FROM HERE
 
 tjet$metadata %>%
-  mutate(table_name = rename_labels(table_name)) %>%
-  filter(incl_prod == 1 & incl_data != "decide") %>%
-  filter(field_type == "multipleSelects")
-
-db$reparations %>% 
-  select(reparationID, legalBasis) %>% 
-  rowwise() %>%
-  mutate(legalBasis = list(make_named_list(legalBasis))) %>%
-  ungroup() %>%
-  unnest_wider(legalBasis, names_sep = "_", simplify = FALSE) %>%
-  mutate(legalBasis_Domestic_law = ifelse(is.na(legalBasis_Domestic_law), 0, 1)) %>%
-  print(n = Inf)
+  filter(incl_prod == 1 & incl_data != "include as is") %>%
+  select(-airtable_record_id, -ID, -table_ID, -field_ID, -last_modified, -created, -field_description, -field_options) %>%
+  select(field_type, incl_data) %>%
+  table()
 
 ### TO DO
 ## - automate variable transformations for all 
@@ -98,19 +104,19 @@ db$reparations %>%
 ##   - for relational DB for browse, we need a separate table for multiselect options 
 ##     - should create this in Airtable 
 
-## create SQLite DB
-# file.remove("tjet.db")
-conn <- dbConnect(RSQLite::SQLite(), "tjet.db")
-lapply(names(tjet)[7:8], function(table_name) {
-  print(table_name)
-  dbWriteTable(conn, table_name, tjet[[table_name]])
-})
-dbListTables(conn) 
-dbListFields(conn, "metadata")
-dbReadTable(conn, "metadata")
-dim(dbGetQuery(conn, "SELECT * FROM metadata"))
-
-result <- dbSendQuery(conn, "SELECT * FROM metadata")
-dbFetch(result)
-dbClearResult(result)
-dbDisconnect(conn)
+### create SQLite DB
+## file.remove("tjet.db")
+# conn <- dbConnect(RSQLite::SQLite(), "tjet.db")
+# lapply(names(tjet)[7:8], function(table_name) {
+#   print(table_name)
+#   dbWriteTable(conn, table_name, tjet[[table_name]])
+# })
+# dbListTables(conn) 
+# dbListFields(conn, "metadata")
+# dbReadTable(conn, "metadata")
+# dim(dbGetQuery(conn, "SELECT * FROM metadata"))
+# 
+# result <- dbSendQuery(conn, "SELECT * FROM metadata")
+# dbFetch(result)
+# dbClearResult(result)
+# dbDisconnect(conn)
