33f1adac4b90dda1f9a77836daa83c0e77d17906
Timo Thoms <timo@geeky.net>
2022-12-03 10:40:47 -0500

    multi-selects (wip)

--- pipeline.R
+++ pipeline.R
@@ -1,48 +1,61 @@
-# devtools::install_github("bergant/airtabler")
-# library(airtabler)
 # remotes::install_github('matthewjrogers/rairtable', ref = 'dev')
-### development version handles multi-select fields without error
+### the development version handles multi-select fields without error
 library(tidyverse)
 library(rairtable)
 library(RSQLite)
-library(parallel)
+# library(parallel)
 
 ### the api key needs to be set only once, is saved to the local R environment
-### if it needs to be set again, replace string and un-comment below accordingly
+### if it needs to be set again, replace string and un-comment below to run once
 # set_airtable_api_key("ACTUAL_API_KEY_GOES_HERE", install = TRUE)
 # readRenviron("~/.Renviron")
 # Sys.getenv("AIRTABLE_API_KEY")
 
-### download data from Airtable 
+### setup
 base_id <- "appHsoHAemKITZgMF"
-names(to_download) <- to_download <- c(
-  "Amnesties",
-  "Trials",
-  "Accused",
-  "TruthCommissions",
-  "Reparations",
-  "Vettings",
-  "CountryYears",
-  "Countries",
-  "Transitions",
-  "Conflicts",
-  "Dyads",
-  "select_options",
-  "metadata")
+names(to_download) <- to_download <- c("Amnesties",
+                                       "Trials",
+                                       "Accused",
+                                       "TruthCommissions",
+                                       "Reparations",
+                                       "Vettings",
+                                       "CountryYears",
+                                       "Countries",
+                                       "Transitions",
+                                       "Conflicts",
+                                       "Dyads",
+                                       "select_options",
+                                       "metadata")
+tableIDs <- c("Amnesties" = "amnestyID",
+              "Trials" = "trialID",
+              "Accused" = "accusedID",
+              "TruthCommissions" = "truthcommissionID",
+              "Reparations" = "reparationID",
+              "Vettings" = "vettingID",
+              "CountryYears" = "countryyearID",
+              "Countries" = "ccode",
+              "Transitions" = "transitionID",
+              "Conflicts" = "conflict_id",
+              "Dyads" = "dyad_id")
+
+### download data from Airtable 
 tjet <- lapply(to_download, function(table) {
   cat(table, "\n") 
   airtable(table, base_id) %>%
     read_airtable(id_to_col = TRUE)
 })
+tjet$Accused$trialID <- as.integer(tjet$Accused$trialID)
 save(tjet, file = "tjet.RData")
 # load("tjet.RData")
 
 ### prodDB tables
-names(select_tables) <- select_tables <- names(tjet)[-length(tjet)]
+names(select_tables) <- select_tables <- names(tjet)[!names(tjet) %in% c("select_options", "metadata")]
 db <- map(select_tables, function(tab_name) {
     # cat(tab_name, "\n", sep = "")
     select_vars <- tjet$metadata %>%
-      filter(incl_prod == 1 & table_name == tab_name) %>%
+      filter(incl_prod == 1 & 
+               table_name == tab_name & 
+               incl_data != "transform: multiple") %>%
       select(field_name) %>%
       unlist(use.names = FALSE)
     select_vars <- c("airtable_record_id", select_vars)
@@ -58,19 +71,98 @@ db <- map(select_tables, function(tab_name) {
 checkbox_to_binary <- function(col) {
   ifelse(is.na(col), 0, 1)
 }
-names(select_tables) <- select_tables <- names(db)[-length(db)]
-db[select_tables] <- map(select_tables, function(tab) {
+db[select_tables] <- map(select_tables, function(tab_name) {
   fields <- tjet$metadata %>%
     filter(incl_prod == 1 & 
              incl_data == "checkmark to binary" & 
-             table_name == tab) %>%
+             table_name == tab_name) %>%
     select(field_name) %>% 
     unlist(use.names = FALSE)
-  db[[tab]][, fields] <- map(db[[tab]][, fields], checkbox_to_binary)
-  return(db[[tab]])
+  db[[tab_name]][, fields] <- map(db[[tab_name]][, fields], checkbox_to_binary)
+  return(db[[tab_name]])
 })
 
-### multiselects
+### multiselect fields
+db[["labels"]] <- tjet$select_options %>%
+  select(labelID, label) %>% 
+  tibble()
+
+multi_selects <- map(select_tables, function(tab_name) {
+  names(fields) <- fields <- tjet$metadata %>%
+    filter(incl_prod == 1 & 
+             incl_data == "transform: multiple" & 
+             table_name == tab_name) %>%
+    select(field_name) %>%
+    unlist(use.names = FALSE)
+  map(fields, function(field) {
+    to_filter_on <- paste(field, "set", sep = "_")
+    to_select <- paste(field, tab_name, sep = "_")
+    tjet$select_options %>%
+      filter(if_all(to_filter_on)) %>%
+      select(all_of(c("labelID", to_select))) %>%
+      unnest_longer(all_of(to_select)) %>%
+      left_join(tjet[[tab_name]] %>%
+                  select(all_of(c("airtable_record_id", tableIDs[[tab_name]]))),
+                by = setNames("airtable_record_id", to_select)) %>%
+      select(all_of(c("labelID", tableIDs[[tab_name]]))) %>%
+      drop_na()
+  })
+}) %>% 
+  unlist(recursive = FALSE) 
+names(multi_selects) <- str_replace(names(multi_selects), fixed("."), "_")
+db <- c(db, multi_selects)
+
+
+
+### FROM HERE
+
+tjet$metadata %>%
+  filter(incl_prod == 1 & 
+           table_name != "select_options") %>%
+  select(-airtable_record_id, -ID, -table_ID, -field_ID, -last_modified, -created, -field_description, -field_options) %>%
+  select(field_type, incl_data) %>%
+  table()
+
+tjet$metadata %>%
+  filter(incl_prod == 1 & 
+           incl_data == "include as key" &
+           table_name != "select_options" & 
+           field_type %in% c("multipleLookupValues", "multipleRecordLinks")) %>%
+  select(table_name, field_name, field_type)
+
+
+
+db$Trials %>% 
+  select(trialID, ccode_Victim1) %>% 
+  unnest_wider(ccode_Victim1, names_sep = "", simplify = TRUE) %>% 
+  rename(ccode_Victim1 = "ccode_Victim11") 
+# %>%
+  # select(ccode) %>% unlist() %>% class()
+
+
+# 12        Amnesties         ccode multipleLookupValues
+# 2       Reparations         ccode multipleLookupValues
+# 7          Vettings         ccode multipleLookupValues
+# 9      CountryYears         ccode multipleLookupValues
+# 10 TruthCommissions         ccode multipleLookupValues
+# 11           Trials   ccode_Trial multipleLookupValues
+# 4            Trials ccode_Accused multipleLookupValues
+
+
+
+# 1            Trials  ccode_Crime1 multipleLookupValues
+# 3            Trials ccode_Victim1 multipleLookupValues
+# 5            Trials ccode_Victim3 multipleLookupValues
+# 6            Trials  ccode_Crime3 multipleLookupValues
+# 8            Trials ccode_Victim2 multipleLookupValues
+# 13           Trials  ccode_Crime2 multipleLookupValues
+
+
+
+### TO DO
+## - turn multi-select fields into dummies for data downloads (needs a consistent naming scheme)
+## - set all PKs and FKs in DB (how to deal with multipleLookupValues & multipleRecordLinks?)
+
 ## don't need this anymore, now that multiselects are turned into separate tables 
 ## but keeping sample code for now
 # make_named_list <- function(lst) {
@@ -87,38 +179,6 @@ db[select_tables] <- map(select_tables, function(tab) {
 #   unnest_wider(legalBasis, names_sep = "_", simplify = FALSE) %>%
 #   mutate(legalBasis_Domestic_law = ifelse(is.na(legalBasis_Domestic_law), 0, 1)) %>%
 #   print(n = Inf)
-
-map(select_tables, function(tab) {
-  fields <- tjet$metadata %>%
-    filter(incl_prod == 1 & 
-             incl_data == "transform: multiple" & 
-             table_name == tab) %>%
-    select(field_name) %>%
-    unlist(use.names = FALSE)
-  tab <- "Amnesties"
-  
-### FROM HERE
-  
-  db$select_options %>%
-    filter(enactedHow_set) %>%
-    select(label, pkey, enactedHow_Amnesties) %>%
-    print(n = 15)
-  
-})
-
-## checking transformations to perform
-tjet$metadata %>%
-  filter(incl_prod == 1) %>%
-  select(-airtable_record_id, -ID, -table_ID, -field_ID, -last_modified, -created, -field_description, -field_options) %>%
-  select(field_type, incl_data) %>%
-  table()
-
-### TO DO
-## - for browse, we need separate tables for one-to-many relationships (i.e. multiselect options) 
-## - turn multi-select fields into dummies for data downloads 
-##   - needs a consistent naming scheme
-## - set all PKs and FKs in DB
-##   - any special cosniderations for multipleLookupValues & multipleRecordLinks
 
 ### create SQLite DB
 ## file.remove("tjet.db")
