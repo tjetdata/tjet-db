e79376636700a52df140ccdcbc99237967d87527
Timo Thoms <timo@geeky.net>
2023-08-04 12:15:06 -0400

    adjusting pipeline to new covariates code (wip)

--- pipeline/analysis_prep.R
+++ pipeline/analysis_prep.R
@@ -1,7 +1,6 @@
 require(tidyverse)
-require(here)
 
-load(here("data", "tjetdb.RData"), verbose = TRUE)
+load(here::here("data", "tjetdb.RData"), verbose = TRUE)
 
 icc <- db[["Accused"]] %>% 
   select(accusedID, trialID, ICC_referral, ICC_prelim_exam, ICC_investigation, 
@@ -30,7 +29,7 @@ icc <- db[["Accused"]] %>%
                          "ICC_confirm_charges", "ICC_proceedings", 
                          "ICC_withdrawnDismissed")), 
                 function(x) ifelse(is.infinite(x), NA, x) )) %>% 
-  distinct() # %>% write_csv(here("data", "tjet_ICC.csv"), na = "")
+  distinct()
 
 ### new TJ variables 
 
@@ -49,7 +48,9 @@ trial_counts <- db[["Trials"]] %>%
   select(ccode_Accused, yearStart, trials_HRs, trials_PostAutocratic, trials_Conflict, trials_HRsConfl, trials_unionFits) %>% 
   distinct() %>% 
   rename(ccode = ccode_Accused, 
-         year = yearStart)
+         year = yearStart) %>% 
+  filter(year >= 1970 & year <= 2020) 
+
 conviction_counts <- db[["Trials"]] %>% 
   mutate(HRs = ifelse(HRs_charges > 0 | humanRights == 1, 1, 0) ) %>%
   select(ccode_Accused, firstConvictionYear_min, HRs, fitsPostAutocraticTJ, fitsConflictTJ) %>%
@@ -65,7 +66,11 @@ conviction_counts <- db[["Trials"]] %>%
   select(ccode_Accused, firstConvictionYear_min, convictions_HRs, convictions_PostAutocratic, convictions_Conflict, convictions_unionFits) %>% 
   distinct() %>% 
   rename(ccode = ccode_Accused, 
-         year = firstConvictionYear_min)
+         year = firstConvictionYear_min) %>% 
+  filter(year >= 1970 & year <= 2020)
+
+counts <- full_join(trial_counts, conviction_counts, by = c("ccode", "year")) %>% 
+  mutate(ccode = ifelse(ccode == 679 & year < 1990, 678, ccode)) # Yemen YAR
 
 # GD new measures: for foreign & intl trials (separately) 
 # –	count of trials (by countryAccused & startYear)
@@ -77,67 +82,64 @@ db[["Accused"]] %>%
   select(accusedID, trialID, ongoing, everGuilty, firstGuiltyYear,
          lastGuilty, lastGuiltyYear, lastSentencingTime, 
          lastSentencingArrangement, lastVerdict, lastVerdictYear) %>% summary 
-  left_join(db[["Trials"]] %>% 
-              select(trialID, ccode_Accused, trialType, yearStart, yearEnd, ongoing) %>% 
-              rename(ongoing_trial = ongoing), 
-            by = "trialID", keep = FALSE) 
+
+  # left_join(db[["Trials"]] %>% 
+  #             select(trialID, ccode_Accused, trialType, yearStart, yearEnd, ongoing) %>% 
+  #             rename(ongoing_trial = ongoing), 
+  #           by = "trialID", keep = FALSE) 
 
 ### merging 
+  
+## this does not work yet, so   
+# download.file(url = "https://github.com/timothoms/country-year-dataset/raw/main/cy_covariates.RData?raw=True", 
+#               destfile = here::here("data", "cy_covariates.RData"), method = "auto", cacheOK = FALSE, 
+#               headers = c(Authorization = "Authorization: token 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", 
+#                           Accept = "Accept: application/vnd.github.v3.raw")) 
 
-read_csv("data/tjet_covariates.csv") %>% 
-  select(-v2x_egaldem, -v2x_egaldem_sd, -v2x_egal, -v2x_egal_sd,
-         -v2juhcind_mean, -v2juncind_mean,
-         -v2exbribe_mean, -v2exbribe, -v2exbribe_sd,
-         -v2exembez_mean, -v2exembez, -v2exembez_sd, 
-         -v2excrptps_mean, -v2excrptps, -v2excrptps_sd, 
-         -v2exthftps_mean, -v2exthftps, - v2exthftps_sd, 
-         -v2lgcrrpt_mean, -v2jucorrdc_mean, -v2mecorrpt_mean) %>% 
-  filter(!(country == "Andorra" & year < 1993)) %>%
-  filter(!(country == "Brunei" & year < 1984)) %>%
-  filter(!(country == "Kiribati" & year < 1997)) %>%
-  filter(!(country == "Liechtenstein" & year < 1990)) %>%
-  filter(!(country == "Marshall Islands" & year < 1991)) %>%
-  filter(!(country == "Micronesia" & year < 1992)) %>%
-  filter(!(country == "Monaco" & year < 1993)) %>%
-  filter(!(country == "Palestinian Territory")) %>%
+df <- readRDS(here::here("data", "cy_covariates.rds")) %>%
+  # mutate(base = TRUE) %>%
   mutate(ccode_cow = ifelse(is.na(ccode_cow) & country == "Soviet Union", 365, ccode_cow), 
          ccode_cow = ifelse(is.na(ccode_cow) & country == "Serbia", 345, ccode_cow), 
          ccode_cow = ifelse(is.na(ccode_cow) & country == "Serbia & Montenegro", 345, ccode_cow), 
          ccode_cow = ifelse(is.na(ccode_cow) & country == "North Vietnam", 816, ccode_cow), 
-         ccode_cow = ifelse(year == 1990 & country == "West Germany", 255, ccode_cow) ) %>%
-  full_join(db[["CountryYears"]] %>% rename(country_cy = country),
-    by = c("ccode_cow" = "ccode", "year" = "year")) %>% 
-  select(-cyID, -country_cy, -ccode_gw, -region, -tjet_focus) %>% 
-  left_join(read_csv("data/tjet_ICC_new_fromGD.csv") %>% 
+         ccode_cow = ifelse(year == 1990 & country == "West Germany", 255, ccode_cow)) %>% 
+  left_join(db[["CountryYears"]] %>% 
+              select(-cyID, -country, -region, -tjet_focus),
+    by = c("ccode_cow" = "ccode", "ccode_ksg" = "ccode_ksg", "year" = "year")) %>% 
+  # losing Slovenia 1991 here
+  full_join(read_csv("data/tjet_ICC_new_fromGD.csv") %>% 
               select(-country, -new, -ICC_atLarge),
             by = c("ccode_cow" = "ccode") ) %>% 
   mutate(ICC_referral = case_when(is.na(ICC_referral) ~ 0, 
-                                  year < ICC_referral ~ 0, 
-                                  year >= ICC_referral ~ 1),
-         ICC_prelim_exam = case_when(is.na(ICC_prelim_exam) ~ 0, 
-                                     year < ICC_prelim_exam | year > ICC_prelimEnd | year > ICC_investigation ~ 0, 
-                                     year >= ICC_prelim_exam ~ 1),
-         ICC_investigation = case_when(is.na(ICC_investigation) ~ 0,
-                                  year < ICC_investigation ~ 0,
-                                  year >= ICC_investigation ~ 1),
-         ICC_arrest_warrant = case_when(is.na(ICC_arrest_warrant) ~ 0, 
-                                       year < ICC_arrest_warrant ~ 0, 
-                                       year >= ICC_arrest_warrant ~ 1),
-         ICC_arrestAppear = case_when(is.na(ICC_arrestAppear) ~ 0, 
-                                        year < ICC_arrestAppear ~ 0, 
-                                        year >= ICC_arrestAppear ~ 1),
-         ICC_confirm_charges = case_when(is.na(ICC_confirm_charges) ~ 0, 
-                                      year < ICC_confirm_charges ~ 0, 
-                                      year >= ICC_confirm_charges ~ 1),
-         ICC_proceedings = case_when(is.na(ICC_proceedings) ~ 0, 
-                                     year < ICC_proceedings | year > ICC_proceedEnd ~ 0, 
-                                     year >= ICC_proceedings & year <= ICC_proceedEnd ~ 1),
-         ICC_withdrawnDismissed = case_when(is.na(ICC_withdrawnDismissed) ~ 0, 
-                                         year < ICC_withdrawnDismissed ~ 0, 
-                                         year >= ICC_withdrawnDismissed ~ 1) ) %>% 
+                                year < ICC_referral ~ 0, 
+                                year >= ICC_referral ~ 1),
+       ICC_prelim_exam = case_when(is.na(ICC_prelim_exam) ~ 0, 
+                                   year < ICC_prelim_exam | year > ICC_prelimEnd | year > ICC_investigation ~ 0, 
+                                   year >= ICC_prelim_exam ~ 1),
+       ICC_investigation = case_when(is.na(ICC_investigation) ~ 0,
+                                     year < ICC_investigation ~ 0,
+                                     year >= ICC_investigation ~ 1),
+       ICC_arrest_warrant = case_when(is.na(ICC_arrest_warrant) ~ 0, 
+                                      year < ICC_arrest_warrant ~ 0, 
+                                      year >= ICC_arrest_warrant ~ 1),
+       ICC_arrestAppear = case_when(is.na(ICC_arrestAppear) ~ 0, 
+                                    year < ICC_arrestAppear ~ 0, 
+                                    year >= ICC_arrestAppear ~ 1),
+       ICC_confirm_charges = case_when(is.na(ICC_confirm_charges) ~ 0, 
+                                       year < ICC_confirm_charges ~ 0, 
+                                       year >= ICC_confirm_charges ~ 1),
+       ICC_proceedings = case_when(is.na(ICC_proceedings) ~ 0, 
+                                   year < ICC_proceedings | year > ICC_proceedEnd ~ 0, 
+                                   year >= ICC_proceedings & year <= ICC_proceedEnd ~ 1),
+       ICC_withdrawnDismissed = case_when(is.na(ICC_withdrawnDismissed) ~ 0, 
+                                          year < ICC_withdrawnDismissed ~ 0, 
+                                          year >= ICC_withdrawnDismissed ~ 1) ) %>% 
   select(-ICC_prelimEnd, -ICC_proceedEnd) %>% 
-  left_join(read_csv("data/icc_statesparty.csv") %>% select(-country), 
-            by = c("ccode_cow" = "ccode", "year" = "year") ) %>% 
+  left_join(read_csv("data/icc_statesparty.csv") %>% 
+              filter(ccode != 511) %>% 
+              mutate(ccode = ifelse(ccode == 260 & year == 1990, 255, ccode)) %>% 
+              select(-country), 
+          by = c("ccode_cow" = "ccode", "year" = "year") ) %>% 
   mutate(sample_trans = ifelse(transition == 1, year, NA),
          sample_confl = ifelse(conflict_active == 1, year, NA)) %>% 
   group_by(ccode_case) %>% 
@@ -152,22 +154,19 @@ read_csv("data/tjet_covariates.csv") %>%
          sample_confl = case_when(is.na(sample_confl) ~ 0, 
                                   year < sample_confl ~ 0, 
                                   year >= sample_confl ~ 1)) %>% 
-  left_join(trial_counts, by = c("ccode_case" = "ccode", "year" = "year") ) %>% 
-  left_join(conviction_counts, by = c("ccode_case" = "ccode", "year" = "year") ) %>% 
+  left_join(counts, by = c("ccode_cow" = "ccode", "year" = "year") ) %>% 
+  ## losing Timor Leste prior to 2002 here; these should be incorporated into Indo
   mutate(across(all_of(c("trials_HRs", "trials_PostAutocratic", "trials_Conflict", 
                          "convictions_HRs", "convictions_PostAutocratic", "convictions_Conflict")), 
                 function(x) ifelse(is.na(x), 0, x))) %>% 
+  ## next four lines need to be incorporated into dataset building script
   mutate(deaths_nonstate_osv = ifelse(year >= 1989 & is.na(deaths_nonstate_osv), 0, deaths_nonstate_osv),
          deaths_state_osv = ifelse(year >= 1989 & is.na(deaths_state_osv), 0, deaths_state_osv), 
-         prev_gov_svac = ifelse(year >= 1989 & !is.na(conflictyear_svac) & is.na(prev_gov_svac), 0, prev_gov_svac),
          prev_nonstate_svac = ifelse(year >= 1989 & !is.na(conflictyear_svac) & is.na(prev_nonstate_svac), 0, prev_nonstate_svac),
-         prev_oth_svac = ifelse(year >= 1989 & !is.na(conflictyear_svac) & is.na(prev_oth_svac), 0, prev_oth_svac),
-         prev_pgm_svac = ifelse(year >= 1989 & !is.na(conflictyear_svac) & is.na(prev_pgm_svac), 0, prev_pgm_svac),
-         prev_reb_svac = ifelse(year >= 1989 & !is.na(conflictyear_svac) & is.na(prev_reb_svac), 0, prev_reb_svac),
          prev_state_svac = ifelse(year >= 1989 & !is.na(conflictyear_svac) & is.na(prev_state_svac), 0, prev_state_svac)) %>% 
   arrange(ccode_case, year) %>% 
-  group_by(ccode_case, isna = is.na(latentmean_fariss) ) %>% 
-  mutate(cum_latentmean_fariss = ifelse(isna, NA, cummean(latentmean_fariss)),
+  group_by(ccode_case, isna = is.na(theta_mean_fariss) ) %>% 
+  mutate(cum_theta_mean_fariss = ifelse(isna, NA, cummean(theta_mean_fariss)),
          sample_combi = ifelse(sample_trans + sample_confl > 0, 1, 0) ) %>% 
   ungroup() %>% 
   select(-isna) %>% 
@@ -196,20 +195,46 @@ read_csv("data/tjet_covariates.csv") %>%
          combi_cum_trials_fits = ifelse(sample_combi == 0, 0, combi_cum_trials_fits),
          combi_cum_convictions_fits = cumsum(convictions_unionFits), 
          combi_cum_convictions_fits = ifelse(sample_combi == 0, 0, combi_cum_convictions_fits)) %>% 
-  ungroup() %>% 
-  write_csv("data/analysis/tjet_analyses.csv", na = "")
+  ungroup()
+
+lags <- df %>%
+  select(-country, -country_name, -histname, -ccode_cow, -ccode_case, -ccode_ksg, 
+         -country_id_vdem, -m49, -isoa3, -cid_who, -micro_ksg,                           
+         -region, -subregion, -intregion, -region_wb, -ldc, -lldc, -sids) %>%
+  mutate(year = year + 1) %>%
+  rename_with(~ paste0("lag_", .x))
+
+df %>%
+  left_join(lags, by = c("country_case" = "lag_country_case", "year" = "lag_year")) %>% 
+  write_csv(here::here("data", "analysis", "tjet_analyses.csv"), na = "")
 
 db[["Amnesties"]] %>%
-  write_csv("data/analysis/tjet_amnesties.csv", na = "")
+  write_csv(here::here("data", "analysis", "tjet_amnesties.csv"), na = "")
 db[["TruthCommissions"]] %>%
-  write_csv("data/analysis/tjet_tcs.csv", na = "")
+  write_csv(here::here("data", "analysis", "tjet_tcs.csv"), na = "")
 db[["Reparations"]] %>%
-  write_csv("data/analysis/tjet_reparations.csv", na = "")
+  write_csv(here::here("data", "analysis", "tjet_reparations.csv"), na = "")
 db[["Trials"]] %>%
-  write_csv("data/analysis/tjet_trials.csv", na = "")
+  write_csv(here::here("data", "analysis", "tjet_trials.csv"), na = "")
 db[["Accused"]] %>%
-  write_csv("data/analysis/tjet_accused.csv", na = "")
+  write_csv(here::here("data", "analysis", "tjet_accused.csv"), na = "")
 db[["Vettings"]] %>%
-  write_csv("data/analysis/tjet_vettings.csv", na = "")
+  write_csv(here::here("data", "analysis", "tjet_vettings.csv"), na = "")
 
-rm(trial_counts, conviction_counts, icc)
+rm(trial_counts, conviction_counts, counts, icc)
+
+## FROM HERE >  
+
+read_csv(here::here("data", "investigations.csv") ) %>% 
+  select(-pkey) %>% 
+  mutate(
+    coi_fact_unga = ifelse(!is.na(coi_fact_unga), 1, 0), 
+    coi_fact_unsc = ifelse(!is.na(coi_fact_unsc), 1, 0), 
+    coi_fact_unhrc = ifelse(!is.na(coi_fact_unhrc), 1, 0), 
+    coi_fact_uchr = ifelse(!is.na(coi_fact_uchr), 1, 0), 
+    coi_fact_unsec = ifelse(!is.na(coi_fact_unsec), 1, 0), 
+    coi_fact_ohchr = ifelse(!is.na(coi_fact_ohchr), 1, 0)) 
+
+# df %>%
+#   mutate(across(all_of(cols), .fns = ~ ifelse(is.na(.x), 0, .x)) ) 
+
