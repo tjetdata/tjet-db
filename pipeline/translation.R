99915c936d9cdf81a7090ff402b97afff38221ab
Timo Thoms <timo@geeky.net>
2023-09-01 18:32:52 -0400

    added sample translation

--- pipeline/translation.R
+++ pipeline/translation.R
@@ -8,16 +8,49 @@ require(deeplr)
 txt <- c("This sentence was brought to you from English to French back to English.", 
          "Transitional justice consists of polices to deal with past human rights abuses.")
 
-translated <- toFrench2(text = txt, 
-                        source_lang = "EN",
-                        auth_key = key_get("DeepL"))
-
-back_trans <- toEnglish2(text = translated, 
-                         source_lang = "FR",
-                         auth_key = key_get("DeepL"))
+# translated <- toFrench2(text = txt, 
+#                         source_lang = "EN",
+#                         auth_key = key_get("DeepL"))
+# back_trans <- toEnglish2(text = translated, 
+#                          source_lang = "FR",
+#                          auth_key = key_get("DeepL"))
 
 # split_text(txt, max_size_bytes = 29000, tokenize = "sentences")
+# pimp2(txt, source_lang = "EN", help_lang = "FR", auth_key = key_get("DeepL"))
+# usage2(key_get("DeepL"))
 
-pimp2(txt, source_lang = "EN", help_lang = "FR", auth_key = key_get("DeepL"))
 
-usage2(key_get("DeepL"))
+### actual 
+
+load(here::here("data", "tjetdb.RData"), verbose = TRUE)
+# str(db, 1)
+db$fr_Countries <- db$Countries 
+
+db$fr_Countries$country <- toFrench2(text = db$fr_Countries$country,
+                                     source_lang = "EN", 
+                                     auth_key = key_get("DeepL"))
+
+db$fr_Countries$txt_intro[!is.na(db$fr_Countries$txt_intro)] <- toFrench2(
+  text = db$fr_Countries$txt_intro[!is.na(db$fr_Countries$txt_intro)],
+  source_lang = "EN", 
+  auth_key = key_get("DeepL"))
+
+db$fr_Countries$txt_regime[!is.na(db$fr_Countries$txt_regime)] <- toFrench2(
+  text = db$fr_Countries$txt_regime[!is.na(db$fr_Countries$txt_regime)],
+  source_lang = "EN", 
+  auth_key = key_get("DeepL"))
+
+db$fr_Countries$txt_conflict[!is.na(db$fr_Countries$txt_conflict)] <- toFrench2(
+  text = db$fr_Countries$txt_conflict[!is.na(db$fr_Countries$txt_conflict)],
+  source_lang = "EN", 
+  auth_key = key_get("DeepL"))
+
+db$fr_Countries$txt_TJ[!is.na(db$fr_Countries$txt_TJ)] <- toFrench2(
+  text = db$fr_Countries$txt_TJ[!is.na(db$fr_Countries$txt_TJ)],
+  source_lang = "EN", 
+  auth_key = key_get("DeepL"))
+
+sample <- db$fr_Countries
+save(sample, file = here::here("data", "fr_sample.RData"))
+
+save(db, file = here::here("data", "tjetdb.RData"))
